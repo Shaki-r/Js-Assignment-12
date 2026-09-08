1cec201e7b2da0ae345321cfb6a242db3e207976
Muhammad Shakir <afridishakir755@gmail.com>
2026-09-09 00:43:50 +0500

    done

--- script.js
+++ script.js
@@ -13,3 +13,33 @@ function activateTodoApp() {
     appTitle.classList.add('show-heading');
   }, 300);
 }
+
+
+
+// --------------functions working----------------------//
+let todoInp = document.querySelector("#todo-input");
+let todoArr = [];
+let todoShow = document.querySelector("#todo-show");
+
+function todoInput(event) {
+        event.preventDefault();
+            todoArr.push(todoInp.value);
+            todoInp.value.innerText="";
+            showTodos();
+     }
+
+     console.log(todoArr)
+
+function showTodos(){
+    todoShow.innerHTML=""
+      for (let i = 0; i < todoArr.length; i++) {
+        todoShow.innerHTML += `${todoArr[i]} <button onclick="deleteTodo(${i})">Delete</button></br>`;
+    }
+}
+// showTodos()
+
+function deleteTodo(index) {
+    todoArr.splice(index, 1); 
+    showTodos(); 
+}
+
